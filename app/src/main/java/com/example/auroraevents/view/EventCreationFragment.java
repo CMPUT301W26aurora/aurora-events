27bb777e3c630313a57d1b7902e9b87c312fd243
Arron <arron.roasa@gmail.com>
2026-03-12 23:56:36 -0600

    Push event creation fragment

---
+++ app/src/main/java/com/example/auroraevents/view/EventCreationFragment.java
@@ -0,0 +1,25 @@
+package com.example.auroraevents.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.auroraevents.R;
+
+public class EventCreationFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_creation, container, false);
+    }
+}
