64582f625c5d2747da9524cfe22ee395e1b5fb4d
sross334 <sross4@ualberta.ca>
2026-03-17 10:01:53 -0600

    Feat qr scanner (#68)
    * Added some scanner functionality
    
    * Cleaned up files to match new layout
    
    * Completed CameraFragment class
    
    * fixed invalid qr code bug
    
    * Added testcases, fixed landscape lock in camera

---
+++ app/src/androidTest/java/com/example/auroraevents/CameraFragmentTest.java
@@ -0,0 +1,89 @@
+package com.example.auroraevents;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.os.Build;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.example.auroraevents.view.CameraFragment;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CameraFragmentTest {
+    @Rule
+    // bypass system notification permission dialogue which may interfere with tests
+    public GrantPermissionRule notificationPermissionRule = Build.VERSION.SDK_INT >= 33
+            ? GrantPermissionRule.grant(android.Manifest.permission.POST_NOTIFICATIONS)
+            : GrantPermissionRule.grant();
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+
+    /**
+     *Tests to see if scanner pops up on button press
+     */
+    @Test
+    public void testScanbtn(){
+
+        onView(withId(R.id.nav_scan)).perform(click());
+        onView(withId(R.id.scan)).check(matches(isDisplayed())).check(matches(isClickable()));
+        onView(withId(R.id.scan)).perform(click()); //the scanner breaks everything in terms of testing, very nice
+    }
+
+    /**
+     * Tests for invalid qr code input
+     */
+    @Test
+    public void testInValid(){
+
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+
+        scenario.onActivity(activity -> {
+
+            CameraFragment fragment = new CameraFragment();
+
+            activity.getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commitNow();
+
+            fragment.handleInvalid();
+        });
+
+        onView(withId(R.id.scan)).check(matches(isDisplayed())); // make sure the
+        //view does not change
+    }
+    @Test
+    public void testValid(){
+
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+
+        scenario.onActivity(activity -> {
+
+            CameraFragment fragment = new CameraFragment();
+
+            activity.getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commitNow();
+
+            fragment.handleValid("test123");
+        });
+
+        onView(withId(R.id.scan)).check(doesNotExist()); // make sure the view changes
+    }
+}
