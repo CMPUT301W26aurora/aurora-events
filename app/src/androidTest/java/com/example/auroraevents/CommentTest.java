680aecd5c669fa43172061c1271d1956f62b42d4
sross334 <sross4@ualberta.ca>
2026-03-28 21:01:20 -0600

    Added unit test files and implemented commentTest

---
+++ app/src/androidTest/java/com/example/auroraevents/CommentTest.java
@@ -0,0 +1,32 @@
+package com.example.auroraevents;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.auroraevents.model.Comment;
+import com.google.firebase.Timestamp;
+
+import org.junit.Test;
+
+public class CommentTest {
+    @Test
+    public void testConstructorAndGetters(){
+        Timestamp now = Timestamp.now();
+        Comment comment = new Comment("Hello", "user", "parent", "id", "event", now, "username");
+
+        assertEquals("Hello", comment.getComment());
+        assertEquals("user", comment.getUserId());
+        assertEquals("parent", comment.getParentId());
+        assertEquals("username", comment.getUsername());
+        assertEquals(now, comment.getTimestamp());
+    }
+
+    @Test
+    public void testGetTimeStampLong() {
+        Timestamp specificTime = new Timestamp(111111, 0); // Example date
+        Comment comment = new Comment();
+        comment.setTimestamp(specificTime);
+
+        long expectedMillis = specificTime.toDate().getTime();
+        assertEquals(expectedMillis, comment.getTimeStampLong());
+    }
+}
