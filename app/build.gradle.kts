7a298bebf7e9397bb70abe767a132dbfa287a227
Arron <arron.roasa@gmail.com>
2026-04-02 21:25:35 -0600

    Remove Google Maps SDK implementation in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,5 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("org.osmdroid:osmdroid-android:6.1.17")
 }
