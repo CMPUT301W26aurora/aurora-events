6429fd030a87e725c301cfc16d700c3b2739ef92
sross334 <sross4@ualberta.ca>
2026-04-05 18:53:27 -0600

    added new listener, changed the frag, added admin frag look

--- app/src/main/java/com/example/auroraevents/view/AdminProfileFragment.java
+++ app/src/main/java/com/example/auroraevents/view/AdminProfileFragment.java
@@ -10,13 +10,50 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.auroraevents.R;
+import com.google.firebase.firestore.ListenerRegistration;
 
 public class AdminProfileFragment extends Fragment {
+    ListenerRegistration listenerRegistration;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_admin_profiles, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_profiles, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if(listenerRegistration != null){
+            listenerRegistration.remove();
+            listenerRegistration = null;
+        }
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        View navBar = getActivity().findViewById(R.id.nav_bar_admin);
+        if (navBar != null) {
+            navBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View nav = getActivity().findViewById(R.id.nav_bar_admin);
+        if (nav != null) {
+            nav.setVisibility(View.GONE);
+        }
     }
 }
