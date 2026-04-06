43804119a9c1bd390f94f199e5a935cb4dc2ec45
sross334 <sross4@ualberta.ca>
2026-04-05 23:12:07 -0600

    added some stuff

--- app/src/main/java/com/example/auroraevents/model/UserAdapterWrapper.java
+++ app/src/main/java/com/example/auroraevents/model/UserAdapterWrapper.java
@@ -8,8 +8,9 @@ import java.util.Date;
 public class UserAdapterWrapper {
     private User user;
     private String status;
-
     private Date date;
+    private String events;
+    private String owned;
     public UserAdapterWrapper(User user, String status, Date date) {
         this.user = user;
         this.status = status;
